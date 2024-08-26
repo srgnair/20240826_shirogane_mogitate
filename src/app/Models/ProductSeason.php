31408609d01f498f6eecd2ae2b3788dfdd3a2a16
shirogane <hirano.11l1252@gmail.com>
2024-08-26 16:55:18 +0900

    Initial commit

---
+++ src/app/Models/ProductSeason.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProductSeason extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'product_id',
+        'season_id'
+    ];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function season()
+    {
+        return $this->belongsTo(Season::class);
+    }
+}
---
+++ src/database/seeders/SeasonsTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class SeasonsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('seasons')->insert([
+            [
+                'name' => '春',
+            ],
+            [
+                'name' => '夏',
+            ],
+            [
+                'name' => '秋',
+            ],
+            [
+                'name' => '冬',
+            ],
+        ]);
+    }
+}
