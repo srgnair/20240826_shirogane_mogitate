24dcf1f88b1f186e10fd7dea4a70dc6635ef8510
shirogane <hirano.11l1252@gmail.com>
2024-08-30 19:37:29 +0900

    ダミーデータ編集

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(ProductsTableSeeder::class);
-        // $this->call(SeasonsTableSeeder::class);
+        $this->call(SeasonsTableSeeder::class);
+        $this->call(ProductSeasonsTableSeeder::class);
     }
 }
